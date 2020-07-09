b71f77fbc68272a1ae37f8fbb981a2502ebbc9d8
Eimert Vink <eimertvink@gmail.com>
2020-07-09 23:41:39 +0200

    Some error handling

--- src/main/java/nl/eimertvink/garageman/controller/InventoryController.java
+++ src/main/java/nl/eimertvink/garageman/controller/InventoryController.java
@@ -6,6 +6,8 @@ import nl.eimertvink.garageman.entity.Warehouse;
 import nl.eimertvink.garageman.repository.InventoryRepository;
 import nl.eimertvink.garageman.util.Endpoints;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,16 +29,17 @@ public class InventoryController {
     }
 
     @RequestMapping(value = "/getById/{id}", method = RequestMethod.GET)
-    public CarDto getById(@PathVariable(value="id") final String id) {
+    public ResponseEntity<CarDto> getById(@PathVariable(value="id") final String id) {
         var warehouse = inventoryRepository.findAll().stream()
             .filter(x -> x.getCars().getCarById(id).isPresent())
             .findFirst();
 
-        return new CarDto(
-            warehouse.orElse(null).getName(),
-            warehouse.orElse(null).getLocation(),
-            warehouse.orElse(null).getCars().getLocation(),
-            warehouse.orElse(null).getCars().getCarById(id).orElse(null)
-        );
+        return warehouse.isPresent() ?
+            new ResponseEntity<>(new CarDto(
+                warehouse.get().getName(),
+                warehouse.get().getLocation(),
+                warehouse.get().getCars().getLocation(),
+                warehouse.get().getCars().getCarById(id).orElse(null)), HttpStatus.OK) :
+            new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
